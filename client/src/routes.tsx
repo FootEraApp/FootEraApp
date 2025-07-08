9d3337d2d4f6029cffb0dadc30384eb5718f6897
Caroline-Silva-FootEra <caroline.silva.footera@gmail.com>
2025-07-08 18:08:46 -0300

    Alguns acertos ds paginas iniciais

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,7 +1,6 @@
 import { Route } from "wouter";
-import PaginaLogin from "@pages/login";
-import PaginaCadastro from "@pages/cadastro";
-import PaginaHome from "@pages/home";
+import PaginaLogin from "./pages/login";
+import PaginaCadastro from "./pages/cadastro";
 import HomeRedirect from "./pages/index";
 
 export function AppRoutes() {
@@ -10,8 +9,6 @@ export function AppRoutes() {
       <Route path="/" component={HomeRedirect} />
       <Route path="/login" component={PaginaLogin} />
       <Route path="/cadastro" component={PaginaCadastro} />
-      <Route path="/home" component={PaginaHome} />
-      <Route>404 - Página não encontrada</Route>
     </>
   );
 }
