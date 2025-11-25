b08d220756fe2de5b614631f2ecd3c602b4e563f
footeraapp <footeraapp@gmail.com>
2025-11-25 19:59:20 -0300

    Fim-25-de-novembro

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-487f81db'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hm6q8f99tk8"
+    "revision": "0.l0br4hdrurg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
