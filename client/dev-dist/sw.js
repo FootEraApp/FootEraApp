abfbf78b669883c9d963ce08616a4855e00db297
footeraapp <footeraapp@gmail.com>
2025-12-15 17:55:13 -0300

    Merge2-15-de-Dezembro

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-487f81db'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lriun0ff3i"
+    "revision": "0.731l347nt68"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
