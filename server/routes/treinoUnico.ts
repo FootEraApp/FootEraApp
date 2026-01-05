9fdf5e49e75481379e7a974c9d0420f7d2d4f5f5
Caroline-Silva-FootEra <caroline.silva.footera@gmail.com>
2026-01-05 20:09:27 -0300

    ajuste contagem de treinos e avaliar treino

--- server/routes/treinoUnico.ts
+++ server/routes/treinoUnico.ts
@@ -1,6 +1,4 @@
-// server/routes/treinoUnico
 import { Router } from "express";
-import { authenticateToken } from "server/middlewares/auth.js";
 import { getTreinoUnico } from "server/controllers/TreinoUnicoController.js";
 
 const router = Router();
