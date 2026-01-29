63135d353e64110b039c063d1262360b6053a924
footeraapp <footeraapp@gmail.com>
2026-01-28 23:39:54 -0300

    Fim-28-de-Janeiro

--- server/routes/turmas.ts
+++ server/routes/turmas.ts
@@ -8,11 +8,14 @@ import {
   listarMinhasTurmas,
   getAlunosTurma,
   deleteTurma,
+  listarTurmasComoProfessor,
+  
 } from "../controllers/turmasController.js";
 import { authenticateToken } from "../middlewares/auth.js";
 
 const router = Router();
 
+router.get("/como-professor", authenticateToken, listarTurmasComoProfessor);
 router.get("/:id/alunos", authenticateToken, getAlunosTurma);
 router.get("/minhas", authenticateToken, listarMinhasTurmas);
 router.post("/:id/alunos", authenticateToken, substituirAlunosTurma);
