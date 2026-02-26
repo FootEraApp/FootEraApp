4742778b365c6020f1190c66edd20f6a93c97ffd
footeraapp <footeraapp@gmail.com>
2026-02-25 21:27:04 -0300

    Fim2-25-de-Fevereiro

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,11 +82,7 @@ define(['./workbox-f8c404e3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-<<<<<<< HEAD
-    "revision": "0.s88l3d3b62o"
-=======
     "revision": "0.v99ikri4sf4"
->>>>>>> 5bb66e763600cc22d1c781a62b906e88b5e13fce
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
