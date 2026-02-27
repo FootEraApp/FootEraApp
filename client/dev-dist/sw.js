fcbc0d59c72cac578532d65a06dab3c867bcbee1
footeraapp <footeraapp@gmail.com>
2026-02-27 01:04:05 -0300

    Merge2-27-de-Fevereiro

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f8c404e3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4t2tbsjlrpk"
+    "revision": "0.r042k36p8d8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
