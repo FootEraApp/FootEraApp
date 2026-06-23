e805174cb1a00127ff664cceac6dfb6118825c1e
Caroline-Silva-FootEra <caroline.silva.footera@gmail.com>
2026-06-23 14:52:32 -0300

    Liga notificacoes web no admin e configuracoes do site

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
 import { AppRoutes } from "./routes.js";
 import { UserProvider } from "./context/UserContext.js";
 import { usePresencePing } from "@/hooks/usePresencePing";
+import { sincronizarPushSePermitido } from "./services/pushNotifications.js";
 
 function PresenceBoot() {
   usePresencePing();
+  return null;
+}
+
+function PushBoot() {
+  useEffect(() => {
+    sincronizarPushSePermitido();
+  }, []);
+
   return null;
 }
 
@@ -11,6 +21,7 @@ export default function App() {
   return (
     <UserProvider>
       <PresenceBoot />
+      <PushBoot />
       <AppRoutes />
     </UserProvider>
   );
